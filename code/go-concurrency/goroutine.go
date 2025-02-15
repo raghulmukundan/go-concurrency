0824c12220105594721b59959712b06faae36275
raghul mukundan <raghulmuk@gmail.com>
2025-02-14 22:12:26 -0600

    expand simple go routine code

--- code/go-concurrency/goroutine.go
+++ code/go-concurrency/goroutine.go
@@ -1,9 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
 func demonstrateGoroutines() {
-    // Each goroutine might only need ~2KB
-    for i := 0; i < 10000; i++ {
-        go func(id int) {
-            fmt.Printf("Goroutine %d running\n", id)
-        }(i)
-    }
-    time.Sleep(time.Second)
+	// Each goroutine might only need ~2KB
+	for i := 0; i < 10000; i++ {
+		go func(id int) {
+			fmt.Printf("Goroutine %d running\n", id)
+		}(i)
+	}
+	time.Sleep(time.Second)
+}
+
+func main() {
+	demonstrateGoroutines()
 }
